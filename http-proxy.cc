aa728c87e76432533667987095ef64684e16938b
Nevinli158 <Nevinli158@gmail.com>
2013-02-03 02:58:06 -0800

    Added base socket code, doesn't compile

--- http-proxy.cc
+++ http-proxy.cc
@@ -1,20 +1,54 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
 #include <iostream>
+#include <cstdlib>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <stdio.h>
+#include <unistd.h> 
+#include "listen.h"
 
 using namespace std;
+
+#define MYPORT "14805"  // the port users will be connecting to
+#define BACKLOG 10     // how many pending connections queue will hold
 
 int main (int argc, char *argv[])
 {
 	/* Listening - Establish a socket connection for listening to incoming connections  (port 14805)
 		Proxy should run with no more than 10 processes, serves at most 10 incoming connections	
 		Once client connected, it should check for proper HTTP request.
-
+		TODO: Need to handle forking max of 10 threads 
 	*/
+	int listen_fd = initServer(MYPORT);
+	if (listen(listen_fd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+	//Get rid of old zombie threads.
+	reapZombies();
+	
+printf("server: waiting for connections...\n");
 
+	int conn_fd;
+    while(1) {  // main accept() loop
+        conn_fd = acceptConnection(listen_fd);
+        if (conn_fd == -1) {
+            perror("accept");
+            continue;
+        }
+		//Fork a child to handle this specific connection
+        if (fork() == 0) { // this is the child process
+            close(listen_fd); // child doesn't need the listener
+			handleConnection(conn_fd);
+        }
+		
+        close(conn_fd);  // parent doesn't need this
+    }
+
+    return 0;	
+	
 	/* Parsing URL
 		Parse the requested URL - 3 things: requested host, port, path
 	*/
---
+++ listen.h
@@ -0,0 +1,17 @@
+#ifndef LISTEN_H
+#define LISTEN_H
+
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+//Creates a socket for the port. Port is a string containing the port number.
+int initServer(const char *port);
+//Initializes addrinfo based on the port.
+struct addrinfo* initAddrInfo(const char *port);
+void reapZombies();
+void sigchld_handler(int s);
+void *get_in_addr(struct sockaddr *sa);
+int acceptConnection(int listen_fd);
+int handleConnection(int conn_fd);
+#endif
