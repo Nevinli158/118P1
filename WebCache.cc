d66d462f5e422189d4b5df26ec0ebfb3cb523f8d
Nevinli158 <Nevinli158@gmail.com>
2013-02-17 17:09:13 -0800

    Cache will now not cache requests that do not have a exptime length

--- WebCache.cc
+++ WebCache.cc
@@ -11,8 +11,13 @@ WebCache::WebCache(){
 
 /*
 	Adds the given page to the cache.
+	expTime has a default value of "".
 */
-void WebCache::cachePage(std::string httpRequest, std::string page, std::string expTime){
+void WebCache::cachePage(const std::string httpRequest, const std::string page, const std::string expTime){
+	//Do not cache the page if an expiration time wasn't specified.
+	if(expTime == ""){
+		return;
+	}
 	m_cacheLock.lockForWrite(); 
 	m_cache.insert(std::pair<std::string,CachedPage>(httpRequest, CachedPage(page, expTime)));
 	m_cacheLock.unlock();
@@ -22,7 +27,7 @@ void WebCache::cachePage(std::string httpRequest, std::string page, std::string 
 	Checks the cache for the given request. Returns 
 	the cached page if it was found, and NULL if it wasn't.
 */
-std::string WebCache::checkCache(std::string httpRequest){
+std::string WebCache::checkCache(const std::string httpRequest){
 	std::map<std::string, CachedPage>::iterator it;
 	
 	m_cacheLock.lockForRead(); 
@@ -47,13 +52,15 @@ std::string WebCache::checkCache(std::string httpRequest){
 /*Returns true if the cached page expired, based on the 
 	current system time.
 */
-bool WebCache::isPageExpired(CachedPage page){
+bool WebCache::isPageExpired(const CachedPage page){
 	time_t currentTime;
 	time(&currentTime);
 	return (difftime(page.m_expireTime, currentTime) < 0);
 }
 
-WebCache::CachedPage::CachedPage(std::string page, std::string expTime){
+
+
+WebCache::CachedPage::CachedPage(const std::string page, const std::string expTime){
 	m_page = page;
 	tm* t = new tm(); 
 	strptime(expTime.c_str(), "%a, %d %b %Y %H:%M:%S",t);
--- WebCache.h
+++ WebCache.h
@@ -12,24 +12,23 @@ class WebCache {
 	public:
 		WebCache();
 		//Adds the page associated with the httprequest to the cache.
-		void cachePage(std::string httpRequest, std::string page, std::string expTime);
+		void cachePage(const std::string httpRequest, const std::string page, const std::string expTime = "");
 		//Checks the cache for the given request, and returns the page if it's available and not timed out.
-		std::string checkCache(std::string httpRequest);
+		std::string checkCache(const std::string httpRequest);
 		
 	private:
 		class CachedPage{
 			public: 
-				CachedPage(std::string page, std::string expTime);
+				CachedPage(const std::string page, const std::string expTime);
 				std::string m_page;
 				time_t m_expireTime;
 		};
 		
 		//Returns whether or not the cached page is expired.
-		bool isPageExpired(CachedPage page);
+		bool isPageExpired(const CachedPage page);
 		
 		std::map<std::string, CachedPage> m_cache;
 		ReadWriteLock m_cacheLock;
-		
 };
 
 #endif
